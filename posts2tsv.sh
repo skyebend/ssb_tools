0bc7edf800f52be9bbcebbc80da346d940265d3f
Skye Bender-deMoll <skyebend@skyeome.net>
2020-07-12 23:25:32 -0700

    removed extra header

--- posts2tsv.sh
+++ posts2tsv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #dump post metadata (without text of post)
-echo -e "key\tprevious\tsequence\tauthor\ttimestamp\troot\tfork\tbranch\tchannel"
+echo -e "key\tprevious\tsequence\tauthor\ttimestamp\troot\tfork\tchannel"
 ssb-server messagesByType --type post --reverse | jq -r '"\(.key)\t\(.value.previous)\t\(.value.sequence)\t\(.value.author)\t\(.value.timestamp)\t\(.value.content.root)\t\(.value.content.fork)\t\(.value.content.channel)"'
